d908bab4514689e08bac8c6710cd34bfbee45542
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-03-10 10:25:09 +0100

    Updates

---
+++ precache-manifest.4f1062c72a54dbfc93947d95f2b5391b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f42af0d1c03f5c854215327fc22a8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e4984dd06116121405c",
+    "url": "./static/css/main.6260a52b.chunk.css"
+  },
+  {
+    "revision": "9b5cf606a4c2bf8d0e11",
+    "url": "./static/js/2.1d86f5cf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.1d86f5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4984dd06116121405c",
+    "url": "./static/js/main.51525775.chunk.js"
+  },
+  {
+    "revision": "13680c27e7588608c979",
+    "url": "./static/js/runtime-main.62a6c3ef.js"
+  }
+]);
